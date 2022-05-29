bd90186497c37300e492d307377db1738fc77bab
ArbiNazari <arbinazari@hotmail.com>
2022-05-28 17:38:50 -0700

    API

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -11,13 +11,11 @@ const {
   removefromFriendList
 } = require('../../controllers/user-controller');
 
-// Set up GET all and POST at /api/users. Provide name of controller as callback
 router
   .route('/')
   .get(getAllUsers)
   .post(createUser);
 
-// Set up GET one, PUT, and DELETE at /api/users/<id>
 router
   .route('/:id')
   .get(getUserById)
@@ -27,7 +25,6 @@ router
 router
   .route('/:userId/friends/:friendId')
   .post(addToFriendList);
-
 
 router
   .route('/:userId/friends/:friendId')
